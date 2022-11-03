16c37520392e7850f85f5f5975c7c42f68d086fd
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-11-03 10:28:58 +0300

    Initial commit

---
+++ 04_scanf.c
@@ -0,0 +1,24 @@
+//
+// Created by Tahsin Elmas on 10/25/22.
+//
+
+
+
+#include <stdio.h>
+
+int main() {
+
+    int i;
+
+    float f;
+
+    char c;
+
+
+    scanf("%d %f %c", &i, &f, &c); // use space
+
+    printf("i = %d\n, f = %f\n, ch= %c", i, f, c);
+
+
+    return 0;
+}
---
+++ DataTypes.c
@@ -0,0 +1,33 @@
+//
+// Created by Tahsin Elmas on 10/25/22.
+//
+#include <stdio.h>
+int main()
+{
+
+    short v3 =31431;
+    float v4 = (float) v3; // Good cast
+
+    printf(" v4 = %f\n",v4);
+
+    char v5 = '2';
+
+    int v6 = (int) v5 ; // Explicit cast
+
+    char v7 = '5';
+
+    int v8 = v7; // Implicit cast
+
+    unsigned v9 = 100u;
+
+    int v10= 0x1743 ; // 1*16^^ +  7*16^0
+
+
+
+
+
+
+
+
+    return 0;
+}
